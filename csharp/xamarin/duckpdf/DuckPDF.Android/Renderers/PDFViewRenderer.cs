5cb9af280c7da8f48d68f390f6bb95ec6fd60211
Marco Olimpi <marco.olimpi@duckpage.com>
2019-02-14 10:52:29 +0100

    Add DuckPDF

---
+++ csharp/xamarin/duckpdf/DuckPDF.Android/Renderers/PDFViewRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Android.Content;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(DuckPDF.PDFView), typeof(DuckPDF.Droid.Renderers.PDFViewRenderer))]
+namespace DuckPDF.Droid.Renderers
+{
+    public class PDFViewRenderer : WebViewRenderer
+    {
+
+        public PDFViewRenderer(Context context): base(context)
+        { 
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var pdfView = Element as PDFView;
+                Control.Settings.AllowUniversalAccessFromFileURLs = true;
+                Control.LoadUrl(string.Format("file:///android_asset/pdfjs/web/viewer.html?file={0}", string.Format("file:///android_asset/Content/{0}", WebUtility.UrlEncode(pdfView.Uri))));
+            }
+        }
+    }
+}
---
+++ csharp/xamarin/duckpdf/DuckPDF.iOS/Renderers/PDFViewRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.IO;
+
+using Foundation;
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(DuckPDF.PDFView), typeof(DuckPDF.iOS.Renderers.PDFViewRenderer))]
+namespace DuckPDF.iOS.Renderers
+{
+    public class PDFViewRenderer : ViewRenderer<DuckPDF.PDFView, UIWebView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<PDFView> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                SetNativeControl(new UIWebView());
+            }
+            if (e.OldElement != null)
+            {
+                // Cleanup
+            }
+            if (e.NewElement != null)
+            {
+                Control.ScalesPageToFit = true;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == DuckPDF.PDFView.UriProperty.PropertyName)
+            {
+                var docView = Element as DuckPDF.PDFView;
+                string fileName = docView.Uri;
+                Control.LoadRequest(new NSUrlRequest(new NSUrl(fileName, false)));
+            }
+        }
+    }
+}
